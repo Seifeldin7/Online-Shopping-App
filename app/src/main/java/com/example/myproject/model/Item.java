1ee4f9aacec231bcb1c0b2e3dcf5e5a4625ca2e2
Nagy <nagyraouf.nr@gmail.com>
2019-12-19 07:49:23 +0200

    Item Class

--- app/src/main/java/com/example/myproject/model/Item.java
+++ app/src/main/java/com/example/myproject/model/Item.java
@@ -1,4 +1,60 @@
 package com.example.myproject.model;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class Item {
+    private int itemID;
+    private String itemName;
+    private int itemImage;
+    private Date date;
+    private double price;
+
+    public Item(int itemID, String itemName, int itemImage, double price) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.itemImage = itemImage;
+        this.price = price;
+        this.date=new Date();
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(int itemImage) {
+        this.itemImage = itemImage;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
