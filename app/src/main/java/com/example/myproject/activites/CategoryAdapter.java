bfa1470a8b2dc6633191abeece0c1dbe50bc54b6
Mohamed Sameh <mohamedsameh1998@gmail.com>
2019-12-22 17:39:48 +0200

    Finish Main Activity

---
+++ app/src/main/java/com/example/myproject/activites/CategoryAdapter.java
@@ -0,0 +1,58 @@
+package com.example.myproject.activites;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.myproject.R;
+import com.example.myproject.model.Category;
+
+public class CategoryAdapter extends BaseAdapter {
+    //source https://www.raywenderlich.com/995-android-gridview-tutorial
+    private final Category[] categories;
+    private final Context mContext;
+
+    public CategoryAdapter(Category[] categories, Context context) {
+        this.categories = categories;
+        this.mContext= context;
+    }
+
+    @Override
+    public int getCount() {
+        return categories.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        final Category category = categories[position];
+
+        //if grid view recycled a cell re-instantiate it
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.category_item, null);
+        }
+
+        TextView text = (TextView) convertView.findViewById(R.id.categoryName);
+        ImageView image = (ImageView) convertView.findViewById(R.id.categoryImage);
+
+        //replace by actual category name from position
+        text.setText("Category 1");
+        image.setImageResource(R.drawable.ic_launcher_foreground);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/myproject/activites/MainActivity.java
+++ app/src/main/java/com/example/myproject/activites/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.myproject.activites;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myproject.R;
+import com.example.myproject.model.Category;
 
 public class MainActivity extends AppCompatActivity {
+    private Category[] categories;
+    String DEBUGTAG = "mainactd";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GridView gridView = (GridView)findViewById(R.id.categoryGridView);
+
+        //call a function to return categories array
+        categories = new Category[10];
+
+
+        for(int i = 0; i < categories.length; i++) categories[i] = new Category();
+        CategoryAdapter categoryAdapter = new CategoryAdapter(categories,this);
+        gridView.setAdapter(categoryAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                Category category = categories[position];
+                Log.d(DEBUGTAG, "listener working");
+                //do an intent based on the category or the id
+
+            }
+//            this function requests a refresh from the grid adapter to update an item in place
+//            categoryAdapter.notifyDataSetChanged();
+        });
+
+
     }
 }
